a85d5ea27304fda8f32a3d2ca0aa22f86c62f2c2
Artur Gharagulyan <argharag@c2r16s5.42yerevan.am>
2025-05-21 19:38:45 +0400

    edded segfoult please ARTUR JAN CHECK YOUR SIGNAL / 256 CASEEEE

--- minishell/src/errors.c
+++ minishell/src/errors.c
@@ -7,13 +7,16 @@ void	ft_errors(int signal, char **back)
 	int	i;
 
 	i = 0;
+	printf("%d\n\n",signal);
 	if (signal == 127 && back[0][0] != '.')
 	{
+		printf("hima stex em\n\n");
 		printf("bash : %s: command not found\n", back[0]);
 		return ;
 	}
 	else if (signal == 127 && back[0][0] == '.')
 	{
+		printf("stex em\n\n");
 		printf("bash : %s: No such file or directory\n", back[0]);
 		return ;
 	}
@@ -22,6 +25,7 @@ void	ft_errors(int signal, char **back)
 		fd = open(back[0], O_RDONLY);
 		if (fd == -1)
 		{
+			printf("lololo\n\n");
 			printf("bash: %s: Permission denied\n", back[0]);
 			return ;
 		}
@@ -29,8 +33,13 @@ void	ft_errors(int signal, char **back)
 		if (line && line[0] == '#' && line[1] == '!')
 			printf("bash : %s: %s: bad interpreter: No such file or directory\n",back[0], line + 2);
 		else
+			{
+			printf("126 -2 \n\n");
 			printf("bash : %s: Permission denied\n", back[0]);
+			}
 		close(fd);
 		free(line);
 	}
+	else if (signal == 139)
+		printf("Segmentation fault (core dumped)\n");
 }
