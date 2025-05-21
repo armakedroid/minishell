904d1eece1c538f8229a6f18773930d4ca64f4a3
Artur Petoyan <apetoyan@c2r16s3.42yerevan.am>
2025-05-21 20:17:37 +0400

    Added echo.c

--- minishell/src/echo.c
+++ minishell/src/echo.c
@@ -1,5 +1,18 @@
 #include "../includes/minishell.h"
 
+static int	need_change(char *back)
+{
+	int	i;
+
+	i = 0;
+	if (back[0] == '\"')
+	{
+		if (back[1] && back[1] == '\'')
+			return (1);
+	}
+	return (0);
+}
+
 void	ft_echo(char **argv)
 {
 	int		i;
@@ -18,16 +31,27 @@ void	ft_echo(char **argv)
 		j = 0;
 		while (back[j])
 		{
-			if(back[j][0] == '$' && back[j][1])
+			printf("back = %s\n", back[j]);
+			if (back[j][0] == '\'' || back[j][ft_strlen(back[j]) - 1] == '\'')
 			{
-				str = getenv(back[j] + 1);
-				if (str)
-					printf("%s", str);
+				str = ft_strtrim(back[j], "'");
+				free(back[j]);
+				back[j] = str;
+				printf("%s",back[j]);
 			}
 			else
-				printf("%s",back[j]);
-			if (back[j + 1] || str)
-				printf(" ");
+			{
+				if(back[j][0] == '$' && back[j][1])
+				{
+					str = getenv(back[j] + 1);
+					if (str)
+						printf("%s", str);
+				}
+				else
+					printf("%s",back[j]);
+				if (back[j + 1] || str)
+					printf(" ");
+			}
 			j++;
 		}
 		free_split(back);
