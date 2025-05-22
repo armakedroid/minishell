a03817688ec58d4755886a501dfee2dec055c819
Artur Gharagulyan <argharag@c2r16s1.42yerevan.am>
2025-05-22 21:26:58 +0400

    added bigsmall function need to add in Makefile in headerfile and start the < > >> << function at the start we need to change minishell.c

---
+++ minishell/src/bigsmall.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bigsmall.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: argharag <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/22 20:58:26 by argharag          #+#    #+#             */
+/*   Updated: 2025/05/22 21:25:10 by argharag         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	small(char **back)
+{
+	int	i;
+	int	j;
+	int	flag;
+
+	flag = 0;
+	i = 0;
+	while (back[i])
+	{
+		j = 0;
+		while (back[i][j])
+		{
+			if (back[i][j] == '<')
+				break;
+			j++;
+		}
+		i++;
+	}
+	fd = open(back[i + 1], O_RDONLY, 0444);
+	if (!fd)
+		ft_errors(127, back);
+	if (dup2(fd, STDIN_FILENO) == -1)
+		ft_errors(127, back);
+	return (0);
+}
+
+int	big(char **back)
+{
+	int	i;
+	int	j;
+	int	flag;
+
+	flag = 0;
+	i = 0;
+	while (back[i])
+	{
+		j = 0;
+		while (back[i][j] && back[i][j + 1])
+		{
+			if (back[i][j] == '>' && back[i][j + 1] == '>')
+				break;
+			j++;
+		}
+		i++;
+	}
+	fd = open(back[i + 1], O_WRONLY | O_CREAT, 0666);
+	if (!fd)
+		ft_errors(127, back);
+	if (dup2(fd, STDOUT_FILENO) == -1)
+		ft_errors(127, back);
+	return (0);
+}
+
+int	big(char **back)
+{
+	int	i;
+	int	j;
+	int	flag;
+
+	flag = 0;
+	i = 0;
+	while (back[i])
+	{
+		j = 0;
+		while (back[i][j])
+		{
+			if (back[i][j] == '>')
+				break;
+			j++;
+		}
+		i++;
+	}
+	fd = open(back[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+	if (!fd)
+		ft_errors(127, back);
+	if (dup2(fd, STDOUT_FILENO) == -1)
+		ft_errors(127, back);
+	return (0);
+}
